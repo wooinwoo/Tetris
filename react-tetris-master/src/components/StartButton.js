6b1f0aa6731015b2ea0cc92d93b4f26d1aee71e2
wooinwoo <wooin1001@gmail.com>
2022-09-21 18:20:12 +0900

    Delete react-tetris-master directory

--- react-tetris-master/src/components/StartButton.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledStartButton } from "./styles/StyledStartButton";
-
-const StartButton = ({ callback }) => (
-  <StyledStartButton onClick={callback}>게임 시작</StyledStartButton>
-);
-
-export default StartButton;
